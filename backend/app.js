d70e9a1098003562fbe89b1120fbb893a17f5880
phamvanhieu3012 <phamvanhieu74123@gmail.com>
2022-04-18 22:57:16 +0700

    Update config app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
@@ -11,6 +12,7 @@ require("dotenv").config({ path: "./config/config.env" });
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // Route Imports
 const product = require("./routes/productRoute");
--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   await mongoose.connect(process.env.DB_URL);
-  console.log("MongoDB connected");
+  console.log("Đã kết nối đến MongoDB");
 };
 
 module.exports = connectDatabase;
