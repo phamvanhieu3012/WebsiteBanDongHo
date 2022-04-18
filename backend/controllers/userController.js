f799ae7db75440c5d3c86ebca4bd8fabf58af99a
phamvanhieu3012 <phamvanhieu74123@gmail.com>
2022-04-18 13:27:36 +0700

    Forgot password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,8 @@ const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 // Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -64,4 +66,77 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
     success: true,
     message: "Bạn đã đăng xuất",
   });
+});
+
+// Forgot Password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHander("User not found", 404));
+  }
+
+  // Get ResetPassword Token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/password/reset/${resetToken}`;
+
+  const message = `Reset token của bạn là :- \n\n ${resetPasswordUrl} \n\nNếu bạn không yêu cầu email này vui lòng bỏ qua nó.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Phục hồi mật khẩu web bán đồng hồ PVH`,
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email đã được gửi tới ${user.email} thành công`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHander(error.message, 500));
+  }
+});
+
+// Reset Password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+  // creating token hash
+  console.log("Hello world");
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHander("Reset Password Token không đúng hoặc đã hết hạn", 400)
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHander("Xác nhận mật khẩu chưa đúng ", 400));
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendToken(user, 200, res);
 });
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,8 @@ const {
   registerUser,
   loginUser,
   logout,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/userController");
 
 const router = express.Router();
@@ -12,5 +14,9 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 router.route("/logout").get(logout);
+
+router.route("/password/forgot").post(forgotPassword);
+
+router.route("/password/reset/:token").put(resetPassword);
 
 module.exports = router;
